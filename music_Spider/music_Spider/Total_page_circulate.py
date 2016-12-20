b5ca7e7e093cd0703a68d703ac0bfe2e5d73e520
tingyun <13284638588@163.com>
2016-12-20 15:07:52 +0800

    update xiami_music crawl

---
+++ music_Spider/music_Spider/Total_page_circulate.py
@@ -0,0 +1,17 @@
+#!coding=utf-8
+
+#这个文件：分离出所有需要计算站点实际page页面的木块，一个可扩展的if elif语句来实现
+
+def Total_page_circulate(site_name,max_pages):
+	if site_name == "xiami_music":
+			return (max_pages/24)+1
+	else:
+			return max_pages
+
+
+
+
+
+
+
+
--- music_Spider/music_Spider/settings.py
+++ music_Spider/music_Spider/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'music_Spider.spiders'
 #USER_AGENT = 'music_Spider (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+#ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
