a60f4ffe81af94217192b157993553a6cd4ee717
tingyun <13284638588@163.com>
2017-01-11 12:23:25 +0800

    use "docker ps" find the dokcer id.

--- music_Spider/run_splash.py
+++ music_Spider/run_splash.py
@@ -6,18 +6,18 @@ import re
 
 splash_image_id = ""
 
-os.system("ps -ef|grep docker > temp.txt")
+os.system("docker ps | grep tingyun > temp.txt")
 with open('temp.txt','r') as f:
 	temp = f.read()
 	try:
-		splash_image_id = re.search('(?<=containerd-shim ).* /',temp).group().replace("/","")
+		splash_image_id = re.search('.{12}',temp).group().replace("/","")
 	except Exception,e:
 		print Exception,":",e
 
 #print "当前的splash id是 : ",splash_image_id,"\n"
 
 if not splash_image_id:
-		os.system("docker run -it -d -p 8050:8050 scrapinghub/splash")
+		os.system("docker run -it -d -p 8050:8050 --name tingyun scrapinghub/splash")
 		print "Splash not exists , start success ....."
 else:
 		os.system("docker restart %s"%splash_image_id)
