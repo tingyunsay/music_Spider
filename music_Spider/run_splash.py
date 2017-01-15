9635826f7f7e5d82ec4f3b003a1431af96f19998
tingyun <13284638588@163.com>
2017-01-15 12:01:35 +0800

    splash自动运行脚本.

--- music_Spider/run_splash.py
+++ music_Spider/run_splash.py
@@ -6,7 +6,7 @@ import re
 
 splash_image_id = ""
 
-os.system("docker ps | grep tingyun > temp.txt")
+os.system("docker ps -a| grep tingyun > temp.txt")
 with open('temp.txt','r') as f:
 	temp = f.read()
 	try:
@@ -17,12 +17,18 @@ with open('temp.txt','r') as f:
 #print "当前的splash id是 : ",splash_image_id,"\n"
 
 if not splash_image_id:
-		os.system("docker run -it -d -p 8050:8050 --name tingyun scrapinghub/splash")
-		print "Splash not exists , start success ....."
+		status_code = os.system("docker run -it -d -p 8050:8050 --name tingyun scrapinghub/splash")
+		if status_code == 0:
+				print "Splash not exists , start success , and named tingyun....."
+		else:
+				print "Error1 , fail!!!"
 else:
-		os.system("docker restart %s"%splash_image_id)
-		print "Splash exists , restart success ....."
-
+		status_code = os.system("docker restart %s"%splash_image_id)
+		if status_code == 0:
+				print "Splash exists , restart tingyun success ....."
+		else:
+				print "Error2 , fail!!!"
+		
 temp_file = './temp.txt'
 
 if os.path.exists(temp_file):
