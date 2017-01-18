8a91ccff93cc6bfe5e051926e4b7668bd26ddbe1
tingyun <13284638588@163.com>
2017-01-18 16:21:24 +0800

    控制脚本，爬虫调度.

--- music_Spider/auto_run.py
+++ music_Spider/auto_run.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-  
 import sys,os
+import re
+import time
+import subprocess
+import commands
 
+def is_runing(spider_name):
+	a = commands.getoutput("ps -ef|grep %s|grep -v grep"%spider_name)
+	return True if a else False	
 
-if len(sys.argv) < 2:
-		print "no spider is target , please check."
-elif sys.argv[1]:
-		spider = sys.argv[1]
-		os.system("nohup scrapy crawl %s >> mv_spider.log 2>&1 &"%spider)
-		print "spider %s is runing ...."%spider
+with open('config.json','r') as f:
+		spider_name = eval(f.read()).keys()
+		for spider in spider_name[2:3]:
+				command = "nohup scrapy crawl %s >> mv_spider.%s.log 2>&1 &"%(spider,(time.strftime("%Y-%m-%d %X",time.localtime())).replace(' ','-'))
+				os.system(command)
+				if is_runing(spider):
+						print "spider %s is runing ...."%spider
+				else:
+						print "spider %s start fail ...."%spider
 
 
